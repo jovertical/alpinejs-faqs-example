88d9f7b193e47f6840d004cdd071b5bcb8c80765
Jovert <jovertical@gmail.com>
2022-01-27 23:52:33 +0800

    completed list

---
+++ components/FaqList.js
@@ -0,0 +1,15 @@
+export default () => ({
+    faqs: [
+        {
+            id: 1,
+            question: 'What is the best thing about Switzerland?',
+            answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat',
+        },
+
+        {
+            id: 2,
+            question: 'How do you make holy water?',
+            answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam aut tempora vitae odio inventore fuga aliquam nostrum quod porro. Delectus quia facere id sequi expedita natus',
+        },
+    ],
+});
--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+import Alpine from 'alpinejs';
+import FaqList from './components/FaqList';
 import './style.css';
 
-document.querySelector('#app').innerHTML = `
-    <h1>Hello Vite!</h1>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+window.Alpine = Alpine;
+
+window.faqList = FaqList;
+
+Alpine.start();
